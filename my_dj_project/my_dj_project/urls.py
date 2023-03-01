c3ede54a308c9e8fe7b88c84f34dfe34ce6e4cff
Iliya Tkachev <xarament@gmail.com>
2023-03-01 19:00:20 +0400

    created CustomerUser and POST models

--- my_dj_project/my_dj_project/urls.py
+++ my_dj_project/my_dj_project/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
@@ -5,7 +7,11 @@ from new_app.views import index
 from django.urls import path, include
 
 urlpatterns = [
+    path("", index),
     path("admin/", admin.site.urls),
     path("new_app/", include("new_app.urls")),
-    path("new_app2/", include("new_app2.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- my_dj_project/new_app/models.py
+++ my_dj_project/new_app/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class CustomerUser(AbstractUser):
+    birth_date = models.DateField(_("birth date"), blank=True, null=True)
+    avatar = models.ImageField(upload_to="profile")
+    about = models.TextField(max_length=500, blank=True)
+
+
+    def __str__(self):
+        return str(self.username)
+
+
